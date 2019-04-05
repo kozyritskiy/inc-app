729c2fef1a9b29a92bace5ae7ebe74f4feb089bf
Maxim Kozyritskiy <maxim.kozyricki@gmail.com>
2019-04-05 13:45:53 +0300

    init: initial dist folder

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,18 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
-};
 
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDSxkClJmEWGJBUiywZf1sgyuojVoLL1kE",
+    authDomain: "inc-app.firebaseapp.com",
+    databaseURL: "https://inc-app.firebaseio.com",
+    projectId: "inc-app",
+    storageBucket: "inc-app.appspot.com",
+    messagingSenderId: "503451180572"
+  }
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
